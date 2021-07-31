05fe55586d95f1bac64bc527ef368532b80813b1
Michał Polak <235046@student.pwr.edu.pl>
2021-07-31 21:41:25 +0200

    added functions summary

---
+++ src/Ip.Obfuscate.Core/Enums/ObfuscationType.cs
@@ -0,0 +1,23 @@
+﻿namespace Ip.Obfuscate.Core.Enums
+{
+    /// <summary>
+    /// Supported obfuscation types.
+    /// <example> For example:
+    /// <code>
+    /// None - no obfuscation;
+    /// Custom - provide which segments of each IP address type should be obfuscated;
+    /// Basic - "111.111.111.111" -> "X11.XXX.XXX.11X";
+    /// Whole - "111.111.111.111" -> "XXX.XXX.XXX.XXX";
+    /// Random - random segments of each IP address type will be obfuscated;
+    /// </code>
+    /// </example>
+    /// </summary>
+    public enum ObfuscationType
+    {
+        None = 0,
+        Custom = 1,
+        Basic = 2,
+        Whole = 3,
+        Random = 4
+    }
+}
--- src/Ip.Obfuscate/IpObfuscator.cs
+++ src/Ip.Obfuscate/IpObfuscator.cs
@@ -1,13 +1,48 @@
-﻿namespace Ip.Obfuscate
+﻿using Ip.Obfuscate.Core.Enums;
+using System.Collections.Generic;
+
+namespace Ip.Obfuscate
 {
     public class IpObfuscator
     {
-        public string ObfuscateIpv4()
+        /// <summary>
+        /// Method to obfuscate Ipv4 addresses.
+        /// If ObfuscationType is Custom, parameter segments cannot be null.
+        /// <example>
+        /// <code>
+        /// segments = new [] {1,3};
+        /// "111.111.111.111" -> "111.XXX.111.XXX"
+        /// </code>
+        /// </example>
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="inputIpAddress"></param>
+        /// <param name="obfuscateChar"></param>
+        /// <param name="obfuscationType"></param>
+        /// <param name="segments"></param>
+        /// <returns></returns>
+        public string ObfuscateIpv4<T>(T inputIpAddress, char obfuscateChar, ObfuscationType obfuscationType, List<int> segments = null)
         {
             return string.Empty;
         }
 
-        private string ObfuscateIpv6()
+        /// <summary>
+        /// Method to obfuscate Ipv6 addresses.
+        /// If ObfuscationType is Custom, parameter segments cannot be null.
+        /// /// <example>
+        /// <code>
+        /// segments = new [] {1,3, 5};
+        /// "2001:0db8:85a3:0000:0000:8a2e:0370:7334" -> "2001:XXXX:85a3:XXXX:0000:XXXX:0370:7334"
+        /// </code>
+        /// </example>
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="inputIpAddress"></param>
+        /// <param name="obfuscateChar"></param>
+        /// <param name="obfuscationType"></param>
+        /// <param name="segments"></param>
+        /// <returns></returns>
+        private string ObfuscateIpv6<T>(T inputIpAddress, char obfuscateChar, ObfuscationType obfuscationType, List<int> segments = null)
         {
             return string.Empty;
         }
