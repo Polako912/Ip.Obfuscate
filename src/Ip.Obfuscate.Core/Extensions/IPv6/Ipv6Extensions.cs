0497dc671597f22740c5a18863d7916a5c884dee
Michał Polak <235046@student.pwr.edu.pl>
2021-08-06 23:34:38 +0200

    work in progress for basic ipv6 obfuscation

--- src/Ip.Obfuscate.Core/Extensions/IPv6/Ipv6Extensions.cs
+++ src/Ip.Obfuscate.Core/Extensions/IPv6/Ipv6Extensions.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text.RegularExpressions;
 
 namespace Ip.Obfuscate.Core.Extensions.IPv6
@@ -22,8 +23,87 @@ namespace Ip.Obfuscate.Core.Extensions.IPv6
             if (string.IsNullOrEmpty(inputIpAddress))
                 return string.Empty;
 
-            return string.Empty;
+            var ipSegments = inputIpAddress.Split(":");
+
+            if (ipSegments.All(s => !CheckSegmentLength(s)))
+                return string.Empty;
+
+            foreach (var ipSegment in ipSegments)
+            {
+
+            }
+
+            var firstSegment = ipSegments.First(x => !string.IsNullOrEmpty(x));
+
+            var lastSegment = ipSegments.Last(x => !string.IsNullOrEmpty(x));
         }
+
+        private static bool CheckSegmentLength(string segment) =>
+            segment.Length > 0;
+
+        private static string ObfuscateFirstIpSegments(string segment, char obfuscateChar)
+        {
+            return segment.Length switch
+            {
+                1 => segment,
+                2 => segment,
+                3 => ObfuscateFirstSegment(segment, obfuscateChar),
+                4 => ObfuscateFirstSegment(segment, obfuscateChar),
+                _ => string.Empty
+            };
+        }
+
+        private static string ObfuscateFirstSegment(string segment, char obfuscateChar)
+        {
+            var charArray = segment.ToCharArray();
+
+            switch (charArray.Length)
+            {
+                case 3:
+                    charArray[2] = obfuscateChar;
+                    break;
+                case 4:
+                    charArray[2] = obfuscateChar;
+                    charArray[3] = obfuscateChar;
+                    break;
+            }
+
+            return new string(charArray);
+        }
+
+        private static string ObfuscateLastIpSegment(string segment, char obfuscateChar)
+        {
+            return segment.Length switch
+            {
+                1 => segment,
+                2 => segment,
+                3 => ObfuscateLastSegment(segment, obfuscateChar),
+                4 => ObfuscateLastSegment(segment, obfuscateChar),
+                _ => string.Empty
+            };
+        }
+
+        private static string ObfuscateLastSegment(string segment, char obfuscateChar)
+        {
+            var charArray = segment.ToCharArray();
+
+            switch (charArray.Length)
+            {
+                case 3:
+                    charArray[0] = obfuscateChar;
+                    break;
+                case 4:
+                    charArray[0] = obfuscateChar;
+                    charArray[1] = obfuscateChar;
+                    break;
+            }
+
+            return new string(charArray);
+        }
+
+        private static string BuildIpString(string firstIpSegment, string lastIpSegment, char obfuscateChar) =>
+            firstIpSegment + "." + StringExtensions
+                .BuildMiddleObfuscatedString(obfuscateChar) + "." + lastIpSegment;
 
         public static string WholeObfuscateExtension(string inputIpAddress, char obfuscateChar)
         {
--- src/Ip.Obfuscate.Tests/Extensions/Ipv6ExtensionsTests.cs
+++ src/Ip.Obfuscate.Tests/Extensions/Ipv6ExtensionsTests.cs
@@ -6,7 +6,15 @@ namespace Ip.Obfuscate.Tests.Extensions
     [TestFixture]
     public class Ipv6ExtensionsTests
     {
+        [TestCase("2001:0db8:85a3:0000:0000:8a2e:0370:7334", '*', ExpectedResult = "20**:****:****:****:****:****:****:**34")]
+        [TestCase("2001:::0000:0000:8a2e:0370:7334", '*', ExpectedResult = "20**:::****:****:****:****:**34")]
+        [TestCase("", '*', ExpectedResult = "")]
+        [TestCase(null, '*', ExpectedResult = "")]
+        public string BasicObfuscateExtension_ValidInout_ReturnsCorrectValue(string inputAddress, char obfuscateChar) =>
+            Ipv6Extensions.BasicObfuscateExtension(inputAddress, obfuscateChar);
+
         [TestCase("2001:0db8:85a3:0000:0000:8a2e:0370:7334", 'X', ExpectedResult = "XXXX:XXXX:XXXX:XXXX:XXXX:XXXX:XXXX:XXXX")]
+        [TestCase("2001:::0000:0000:8a2e:0370:7334", '*', ExpectedResult = "****:::****:****:****:****:****")]
         [TestCase("", '*', ExpectedResult = "")]
         [TestCase(null, '*', ExpectedResult = "")]
         public string WholeObfuscateExtension_ValidInout_ReturnsCorrectValue(string inputAddress, char obfuscateChar) =>
